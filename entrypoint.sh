cca5ec0ac5c2ea606d08e3685f39a4479f87de36
Athos Ribeiro <athos.ribeiro@canonical.com>
2021-09-02 16:19:58 -0300

    Create missing cache directories
    Ensure the needed cache directories are created before starting the
    service
    
    Signed-off-by: Athos Ribeiro <athos.ribeiro@canonical.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,4 +20,6 @@ tail -F /var/log/squid/access.log 2>/dev/null &
 tail -F /var/log/squid/error.log 2>/dev/null &
 tail -F /var/log/squid/store.log 2>/dev/null &
 tail -F /var/log/squid/cache.log 2>/dev/null &
+# create missing cache directories and exit
+/usr/sbin/squid -Nz
 /usr/sbin/squid "$@"
